a87ac0e81d09c568f5ba0378503f335f52b93aef
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-10-27 00:56:42 -0400

    Topografia e realizando correção bugs de responsividade

--- js/main.js
+++ js/main.js
@@ -1,29 +1,46 @@
+new SimpleAnime();
+
+/* SLIDE  */
+
 let time = 5000,
     currentImageIndex = 0;
 images = document.querySelectorAll(".apresentacao img");
+color = document.querySelectorAll(".listaButonApresentacao li")
 max = images.length;
-var myVar = setInterval(() => {nextImage()}, time)
+var myVar = setInterval(() => { nextImage() }, time)
 
 
-function stopInteval(){
+
+function stopInteval() {
     clearInterval(myVar)
 }
 
 function nextImage() {
     images[currentImageIndex].classList.remove("selected")
+    color[currentImageIndex].classList.remove("selectedColor")
+
     currentImageIndex++
     if (currentImageIndex >= max) {
         currentImageIndex = 0;
     }
     images[currentImageIndex].classList.add("selected")
+    color[currentImageIndex].classList.add("selectedColor")
 }
 
-function butonApresentacao(posicao){
+function butonApresentacao(posicao) {
     images[currentImageIndex].classList.remove("selected")
+    color[currentImageIndex].classList.remove("selectedColor")
     currentImageIndex = posicao;
     images[currentImageIndex].classList.add("selected")
+    color[currentImageIndex].classList.add("selectedColor")
     stopInteval()
-    myVar = setInterval(() => {nextImage()}, time)
+    myVar = setInterval(() => { nextImage() }, time)
 }
+/* FIM SLIDE  */
 
-window.addEventListener("load", myVar);
+
+
+
+
+
+
